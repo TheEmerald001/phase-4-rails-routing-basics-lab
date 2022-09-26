99ae738cc6f795db74e3812dd84964da3620928a
steve <austines811@gmail.com>
2022-09-26 13:40:23 +0300

    commit tree b420a703dcff65c5d1459b7efb71c9d71371ed25

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  #GETs from /students
+  get "students", to: 'student#index'
+  #GETs from /students/grades
+  get "students/grades", to:'student#grades'
+  #GETs highest_grade student
+  get "students/highest-grade", to:'student#highest_grade'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
